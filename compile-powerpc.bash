eaaf0b397affbcef03acbbcc45b924101696a636
textbrowser <textbrowser@gmail.com>
2022-05-25 10:30:24 -0400

    New file.

---
+++ compile-powerpc.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Example only!
+
+rm -f test
+
+g++	-fPIC \
+	-Wall \
+	-Wcast-qual \
+	-Wconversion \
+	-Werror \
+	-Wextra \
+	-Wformat=2 \
+	-Wformat-security \
+	-Wno-unused-function \
+	-Wno-unused-variable \
+	-Wpedantic \
+	-Wpointer-arith \
+	-Wshadow \
+	-Wsign-conversion \
+	-Wstrict-overflow=5 \
+	-Wundef \
+	-pedantic \
+	-std=c++17 \
+	test.cc -o test
