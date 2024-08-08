09f2dda9a3896614cce7b90770eb52e059257d22
textbrowser <textbrowser@gmail.com>
2024-08-08 16:35:19 -0400

    Compiler options.

--- compile.bash
+++ compile.bash
@@ -4,25 +4,26 @@
 
 rm -f test
 
-c++	-fPIC \
-	-Wall \
-	-Walloca \
-	-Wcast-qual \
-	-Wconversion \
-        -Wdangling-reference \
-	-Werror \
-	-Wextra \
-	-Wformat=2 \
-	-Wformat-security \
-	-Wno-unused-function \
-	-Wno-unused-variable \
-	-Wnull-dereference \
-	-Wpedantic \
-	-Wpointer-arith \
-	-Wshadow \
-	-Wsign-conversion \
-	-Wstrict-overflow=5 \
-	-Wundef \
-	-pedantic \
-	-std=c++17 \
-	test.cc -o test
+c++ -fPIC \
+    -Wall \
+    -Walloca \
+    -Warith-conversion \
+    -Wcast-qual \
+    -Wconversion \
+    -Wdangling-reference \
+    -Werror \
+    -Wextra \
+    -Wformat=2 \
+    -Wformat-security \
+    -Wno-unused-function \
+    -Wno-unused-variable \
+    -Wnull-dereference \
+    -Wpedantic \
+    -Wpointer-arith \
+    -Wshadow \
+    -Wsign-conversion \
+    -Wstrict-overflow=5 \
+    -Wundef \
+    -pedantic \
+    -std=c++17 \
+    test.cc -o test
